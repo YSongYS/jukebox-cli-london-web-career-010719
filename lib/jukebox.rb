42604874cd438f0207d373eae6bde370b94bad17
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-06 15:30:44 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,8 +1,31 @@
-def say_hello(name)
-  "Hi #{name}!"
+def help
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
 end
 
-puts "Enter your name:"
-users_name = gets.chomp
+def list
+  puts "1. Phoenix - 1901
+2. Tokyo Police Club - Wait Up
+3. Sufjan Stevens - Too Much
+4. The Naked and the Famous - Young Blood
+5. (Far From) Home - Tiga
+6. The Cults - Abducted
+7. Phoenix - Consolation Prizes
+8. Harry Chapin - Cats in the Cradle
+9. Amos Lee - Keep It Loose, Keep It Tight"
+end
 
-puts say_hello(users_name)
+def play (songs)
+  puts "Please enter a song name or number:"
+  user_input = gets.chomp
+  if user_input.to_i.is_a?(Integer) && user_input.to_i<songs.size && user_input.to_i>0
+    puts "Playing #{songs[user_input-1]}"
+  elsif songs.include?(user_input)
+    puts "Playing #{user_input}"
+  else
+    puts "Invalid input, please try again"
+
+end
